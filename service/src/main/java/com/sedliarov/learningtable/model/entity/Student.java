14529a59dae8fde35c2c159fe2dbcfd7554aab0e
Kirill <sedliarov78@gmail.com>
2022-09-28 21:00:44 +0300

    Corrected checkstyle.

--- service/src/main/java/com/sedliarov/learningtable/model/entity/Student.java
+++ service/src/main/java/com/sedliarov/learningtable/model/entity/Student.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.UUID;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
